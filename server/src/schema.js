6b68f9b8e316cb71523c41e746c6b0d7276e0631
Karol Wons <karol.wons@collibra.com>
2021-11-18 10:22:40 +0100

    lift off part 3 complete code

--- server/src/schema.js
+++ server/src/schema.js
@@ -7,6 +7,12 @@ const typeDefs = gql`
     photo: String
   }
 
+  type Module {
+    id: ID!
+    title: String!
+    length: Int
+  }
+
   type Track {
     id: ID!
     title: String!
@@ -14,10 +20,14 @@ const typeDefs = gql`
     thumbnail: String
     length: Int
     modulesCount: Int
+    description: String
+    numberOfViews: Int
+    modules: [Module!]!
   }
 
   type Query {
     tracksForHome: [Track!]!
+    track(id: ID!): Track!
   }
 `;
 
